6baeecfff8180671c4c6d5111288b3fff7d857d9
Zuotian Tatum <z.tatum@lumc.nl>
2012-10-08 17:31:11 +0200

    Added assembly URI and details of reference sequence URIs.

--- scripts/converters/assembly_report_converter.rb
+++ scripts/converters/assembly_report_converter.rb
@@ -1,43 +1,93 @@
 # take a genome assembly report and convert it to triples.
+# triples include a URI to the genome assembly, and URIs to all reference sequences that are part of this
+# genome build.
 # you can find assembly reports at ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/
 
 require 'rdf'
 require 'rdf/turtle'
 require 'optparse'
+require 'titlecase'
+
 
 # namespaces
 DC = RDF::DC
 RDFS = RDF::RDFS
 XSD = RDF::XSD
 RS = RDF::Vocabulary.new('http://rdf.biosemantics.org/ontologies/ReferenceSequence#')
-$base = RDF::Vocabulary.new('http://rdf.biosemantics.org/data/genome/assembly/hg19#')
+GB = RDF::Vocabulary.new('http://rdf.biosemantics.org/ontologies/GeneBank#')
+HG = RDF::Vocabulary.new('http://rdf.biosemantics.org/data/genomes/humangenome#')
 
-prefixes = {
+$prefixes = {
     :dcterms => DC,
     :rdf => RDF,
     :xsd => XSD,
     :rdfs => RDFS,
     :rs => RS,
-    nil => $base
+    :gb => GB,
+    :hg =>HG
 }
 
-graph = RDF::Graph.new($base)
+# say something about the genome assembly.
 
 # @param [Object] row
-def convert(graph, row)
-  name, role, cp, genebank_accn, refseq_accn, unit = row.strip.split("\t")
-  if unit == 'Primary Assembly'
-    graph << [$base[name], RDF.type, RS.ReferenceSequence]
-  elsif unit == 'non-nuclear'
+def convert_reference_sequence(graph, row, base)
+  name, role, cp, genebank_accn, refseq_accn, unit = row.split("\t")
+  if unit == 'Primary Assembly' or unit == 'non-nuclear'
+    graph << [base[name], RDF.type, RS.ReferenceSequence]
+    graph << [base[name], DC.partOf, base['Assembly']]
+    graph << [base[name], RS.role, role]
+    graph << [base[name], RS.assemblyUnit, unit]
+    if not cp.empty?
+      graph << [base[name], RS.locatesIn, HG['chr' + cp]]
+    end
+    graph << [base[name], RDFS.seeAlso, RDF::URI.new('http://www.ncbi.nlm.nih.gov/nuccore/' + genebank_accn)]
+    graph << [base[name], RDFS.seeAlso, RDF::URI.new('http://www.ncbi.nlm.nih.gov/nuccore/' + refseq_accn)]
+  end
+end
 
+def annotate_assembly(graph, row, base)
+  if row =~ /\#\ ([^\:]+)\:\s*(.+)/
+    description, value = $1, $2
+    graph << [base['Assembly'], GB[description.titlecase.gsub(/\s/, '')], value]
+  else
+    puts 'row not conforming to format, ignore'
+  end
+end
+
+
+def convert(options)
+  base = RDF::Vocabulary.new('http://rdf.biosemantics.org/data/referencesequences/' + options[:name] + '#')
+
+  graph = RDF::Graph.new(base)
+  $prefixes[nil] = base
+  graph << [base['Assembly'], RDF.type, RS.GenomeAssembly]
+
+  File.open(options[:input], 'r') do |f|
+    while line = f.gets
+      if line[0] == '#'
+        annotate_assembly(graph, line.strip, base)
+      else
+        convert_reference_sequence(graph, line.strip, base)
+      end
+    end
+  end
+
+  RDF::Turtle::Writer.open('assembly.test.ttl') do |writer|
+    writer.prefixes = $prefixes
+    writer.base_uri = base
+    writer << graph
   end
 end
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: assembly_report_converter.rb -i xxx.assembly.txt -o xxx.assembly.ttl"
+  opts.banner = "Usage: assembly_report_converter.rb -n hg19 -i xxx.assembly.txt -o xxx.assembly.ttl"
 
   # Mandatory argument.
+  opts.on("-n", "--name NAME") do |name|
+    options[:name] = name
+  end
+
   opts.on("-i", "--input ASSEMBLY") do |input|
     options[:input] = input
   end
@@ -53,16 +103,5 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-File.open(options[:input], 'r') do |f|
-  while line = f.gets
-    if line[0] != '#'
-        convert(graph, line)
-    end
-  end
-end
-
-RDF::Turtle::Writer.open('assembly.test.ttl') do |writer|
-  writer.prefixes = prefixes
-  writer.base_uri = $base
-  writer << graph
-end
+# call main
+convert(options)
